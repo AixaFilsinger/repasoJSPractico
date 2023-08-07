dc48e482035e4444192cd8959a31c951237a170b
Aixa Filsinger <macafil99@gmail.com>
2023-08-06 22:12:29 -0300

    se realizo los ejercicios 2,3,4 y 5

---
+++ ejercicio5.js
@@ -0,0 +1,6 @@
+//5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+let numero1 = parseInt(prompt("ingrese un nuemero a sumar"));
+let numero2 = parseInt(prompt("ingrese un nuemero a sumar"));
+let suma = numero1+numero2;
+
+document.write("El resultado de sumar "+numero1+ " y " + numero2+ " es: "+ suma);
